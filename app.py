6959e25ae530756c5f0b8be7a28a1f7772bba796
ADOBENET\hnguyen <hnguyen@adobe.com>
2020-10-26 02:24:52 -0700

    add db conneciton

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+import os
 
-@app.route("/")
-def hello():
-    return "Hello, Pamplemouse updated into main project!"
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+database_uri = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+)
+
+app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=database_uri,
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+# initialize the database connection
+db = SQLAlchemy(app)
+
+# initialize database migration management
+migrate = Migrate(app, db)
+
+
+@app.route('/')
+def home():
+    from models import Recipe
+    recipes = Recipe.query.all()
+    return render_template('recipe_list.html', recipes=recipes)
