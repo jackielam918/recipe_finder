01b236b1d2444f9e1ec3f24b5cda615e3c9536b5
jackie lam <jlam61@gatech.edu>
2020-11-18 22:16:52 -0800

    cleaned things up

--- backend/backend_scrap.py
+++ backend/backend_scrap.py
@@ -3,6 +3,6 @@ from modeling.ingredient_embedder import IngredientEmbedderWrapper
 model_path = '/home/jackielam/Documents/OMSA/fall_2020/dva/DVA_Project/backend/modeling/outputs/11411120'
 model = IngredientEmbedderWrapper(model_path)
 
-# model.most_similar_recipe([28, 103, 321, 1206, 3956, 4848, 5925], 0.5)
-x = {"recipe": [9], "ingredients": [8]}
-model.get_substitute_ingredients(**x)
+model.most_similar_recipe([28, 103, 321, 1206, 3956, 4848, 5925], 0.5)
+
+
