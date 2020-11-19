b4015aa464d3b793e5d343c2c3024eff942cb1fd
jackie lam <jlam61@gatech.edu>
2020-11-18 22:55:39 -0800

    added dockerfile

--- backend/modeling/create_visualizations.py
+++ backend/modeling/create_visualizations.py
@@ -1,17 +1,7 @@
-# OLD CODE
-from modeling.model_trainer import IngredientEmbedder
-from data_munging.data_prep import DataHandler
-import pandas as pd
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-corpus = pd.read_pickle('/data_munging/data/recipe_data.pkl')
 
-corpus = corpus
-data_handler = DataHandler()
-data_handler.create_co_occurrence_matrix(corpus)
-model = IngredientEmbedder(data_handler=data_handler, alpha=0.75, output_dim=50, x_max=100)
-model.fit(corpus=corpus, epochs=100, lr=0.5, batch_size=4096)
 names = [n for n, i in model.most_similar_ingredients('sugar', 20)]
 
 tsne = TSNE(n_components=2)
